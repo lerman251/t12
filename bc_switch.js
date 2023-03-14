943fc7f93c8f6b2aa9c48daca71bb778cde67b53
lerman251 <lthompson2023@gmail.com>
2023-03-14 10:18:03 -0500

    dylan sucks at coding

--- bc_switch.js
+++ bc_switch.js
@@ -17,3 +17,60 @@
    
 */
 
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+    var pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href", "bc_page.css");
+    pageStyle.setAttribute("rel", "stylesheet");
+    pageStyle.setAttribute("disabled", "disabled");
+
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled = true;
+
+    var buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id", "styleButtons");
+
+    var webButton = document.createElement("input");
+    webButton.setAttribute("type", "button");
+    webButton.setAttribute("value", "Web View");
+
+    var pageButton = document.createElement("input");
+    pageButton.setAttribute("type", "button");
+    pageButton.setAttribute("value", "Page View");
+
+    buttonDIV.appendChild(webButton);
+    buttonDIV.appendChild(pageButton);
+
+    document.body.insertBefore(buttonDIV, document.body.firstChild);
+
+    var buttonStyles = document.createElement("style");
+    document.head.appendChild(buttonStyles);
+
+    document.styleSheets[document.styleSheets.length-1].insertRule(
+        "div#styleButtons { \
+            position: fixed; \
+        }", 0);
+
+        document.styleSheets[document.styleSheets.length-1].insertRule(
+            "div#styleButtons input { \
+                background-color: rgba(68, 94, 186, 0.6); \
+                border: 3px solid rgba(0, 24, 123, 0.6); \
+                border-radius: 50%; \
+                cursor: pointer; \
+                color: white; \
+                display: inline-block; \
+                font-size: 1.2em; \
+                height: 60px; \
+                margin: 5px 10px; \
+                width: 100px; \
+            }", 1);
+
+            document.styleSheets[document.styleSheets.length-1].insertRule(
+                "@media print { \
+                    div#stylesButtons { \
+                        display: none; \
+                    } \
+                }", 2);
+                
+}
